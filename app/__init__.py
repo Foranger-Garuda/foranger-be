0b833ff08b9db512df4757206df26af7d2770763
photic23 <resellcourtesyx@gmail.com>
2025-07-25 02:08:07 +0700

    added cors

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from app.extensions import db, jwt
 from app.routes import main_bp
 from models import *
 from app.services.auth_service import is_token_revoked
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
@@ -13,6 +14,9 @@ def create_app():
     db.init_app(app)
     migrate = Migrate(app, db)
     jwt.init_app(app)
+
+    # Enable CORS
+    CORS(app, supports_credentials=True)
 
     @jwt.token_in_blocklist_loader
     def check_if_token_revoked(jwt_header, jwt_payload):
